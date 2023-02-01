f51345fd70d8f68208c22ef9c1a16c88bd1c6ff4
Neil99-byte <77081903+Neil99-byte@users.noreply.github.com>
2023-02-01 14:18:20 +0000

    Delete Scripts directory

--- Scripts/Data/RegisterData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RegisterData 
-{
-    public Dictionary<string, string> registerInfo = new Dictionary<string, string>();
-}
